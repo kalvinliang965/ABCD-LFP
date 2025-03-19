ab222dd6a26b1792d6c8cf9df1563f27280fee5e
Kalvin Liang Wu <kalvinliang123@gmail.com>
2025-03-18 23:41:21 -0400

    finish scraping tax bracket table

--- server/src/core/tax/FederalTaxScraper.ts
+++ server/src/core/tax/FederalTaxScraper.ts
@@ -1,45 +1,84 @@
 import axios from "axios";
 import "cheerio";
 import { load } from "cheerio";
+import { TaxBrackets, TaxBracketsObject } from "./TaxBrackets";
+import { TaxFilingStatus } from "../Enums";
 
 const SINGLE_TABLE: number = 0;
-const MARRIED_JOINT_TABLE: number = 1;
+const MARRIED_TABLE: number = 1;
 const FEDERAL_TAX_URL: string = "https://www.irs.gov/filing/federal-income-tax-rates-and-brackets";
 
-async function scrape_federal_tax_tables(url: string) {
-    const federal_tax_tables = new Array();
-    try {
-        const  { data: html } = await axios.get(url)
-        const $ = load(html);
+// Function to parse table rows and extract tax bracket data
+function parse_table_rows(taxBrackets: TaxBracketsObject, status: TaxFilingStatus, table: string): void {
+    const $ = load("<table>" + table + "</table");
+    // remove header row
+    const rows = $("tr").slice(1);
+    rows.each((idx, row) => {
+        const cells = $(row).find('td');
+        if (cells.length < 3) return;
+        const rate_text = $(cells[0]).text().trim();
+        const min_text = $(cells[1]).text().trim();
+        const max_text = $(cells[2]).text().trim();
+        const min = cleanCurrency(min_text);
+        const max = max_text.toLowerCase() === "over" ? Infinity: cleanCurrency(max_text);
+        const rate = cleanPercentage(rate_text);
+        taxBrackets.add_rate(min, max, rate, status);
+    });
+}
 
+// Function to parse federal tax tables
+function parse_federal_tax_tables(tables: Array<string>) {
+    if (!tables || tables.length < 4) {
+        throw new Error("Not enough table found");
+    }
+    const taxBrackets = TaxBrackets();    
+    const single_table = tables[SINGLE_TABLE];
+    if (!single_table) {
+        throw new Error("Missing single taxpayer table");
+    }
+    parse_table_rows(taxBrackets, TaxFilingStatus.SINGLE, single_table);
+    const married_table = tables[MARRIED_TABLE];
+    if (!married_table) {
+        throw new Error("Misssing married table");
+    }
+    parse_table_rows(taxBrackets, TaxFilingStatus.MARRIED, married_table);
+    return taxBrackets;
+}
+
+// Function to fetch and parse the federal tax brackets
+async function parse_federal_tax_brackets(): Promise<TaxBracketsObject> {
+    try {
+        const  { data: html } = await axios.get(FEDERAL_TAX_URL);
+        const $ = load(html);
+        const federal_tax_tables: string[] = new Array();
         $('table').each((_, dom_element) => {
             const table = $(dom_element);
-            federal_tax_tables.push(table);
-            
+            const table_str = table.html();
+            if (!table_str) {
+                throw new Error("Missing a table");
+            }
+            federal_tax_tables.push(table_str);
         });
+        
+        if (federal_tax_tables.length < 4) {
+            throw new Error("Not enough table found");
+        }
+        return parse_federal_tax_tables(federal_tax_tables);
     } catch (error) {
-        console.error("Error fetching the federal tax URL:", error);
+        console.error("Error fetching the federal tax brackets:", error);
+        throw new Error("Federal tax data unavailable. Try again Later");
     }
-    return federal_tax_tables;
 }
 
-function parse_table(table: cheerio.Cheerio) {
-    
-}
+const cleanCurrency = (text: string): number => 
+    parseFloat(text.replace(/[^0-9.]/g, ''));
 
+const cleanPercentage = (text: string): number =>
+    parseFloat(text.replace('%', '')) / 100;
 
 async function main() {
-
-    const federal_tax_table = await scrape_federal_tax_tables(FEDERAL_TAX_URL)
-   
-    const single_table = federal_tax_table[SINGLE_TABLE];
-    const $single = load(single_table.html());
-    console.log(single_table.text());
-
-    const married_joint_table = federal_tax_table[MARRIED_JOINT_TABLE];
-    const $married = load(married_joint_table.html());
-    console.log(married_joint_table.text());
+    const taxBracket = await parse_federal_tax_brackets();
+    console.log(taxBracket.to_string());
 } 
 
 main()
-console.log("HELLO")
--- server/src/core/tax/TaxBrackets.ts
+++ server/src/core/tax/TaxBrackets.ts
@@ -1,51 +1,103 @@
 import { TaxFilingStatus } from "../Enums";
 
-export function TaxBrackets() {
+export type TaxBracket = {
+    min: number,
+    max: number,
+    rate: number,
+} 
+
+export type TaxBracketSet = TaxBracket[];
+
+export interface TaxBracketsObject {
+    add_rate: (min: number, max: number, rate: number, status: TaxFilingStatus) => void;
+    find_rate: (income: number, status: TaxFilingStatus) => number;
+    adjust_for_inflation: (rate: number) => void;
+    to_string: () => string;
+};
+
+export function TaxBrackets(): TaxBracketsObject {
 
     // single tax payer bracket
     // married filing jointly brakcet
-    const brackets: Map<TaxFilingStatus, Map<[number, number], number>> = new Map();
-    brackets.set(TaxFilingStatus.SINGLE, new Map());
-    brackets.set(TaxFilingStatus.MARRIED, new Map());
-
-    const adjust_for_inflation = (rate: number) => {
-        for (let [_, bracket] of brackets) {
-            for (let [key, val] of bracket) {
-                bracket.set(key, val * (1 + rate));
-            }
+    const brackets: Map<TaxFilingStatus, TaxBracketSet> = new Map ([
+        [TaxFilingStatus.SINGLE, []],
+        [TaxFilingStatus.MARRIED, []],
+    ]);
+    
+    const add_rate = (
+        min: number,
+        max: number,
+        rate: number, 
+        status: TaxFilingStatus,
+    ): void => {
+        const bracketSet = brackets.get(status); 
+        if (!bracketSet) {
+            throw new Error(`Invalid TaxFilingStatus: ${status}`);
         }
+        
+        if (min >= max || min < 0) {
+            throw new Error(`Invalid tax bracket [${min}, ${max}]`);
+        }
+        const newBracket: TaxBracket = {min, max, rate };
+        bracketSet.push(newBracket);
+        // sort it for easier retrieve
+        bracketSet.sort((a, b) => a.min - b.min); 
     }
 
-
-    const add_rate = (lowerbound: number, upperbound: number, rate: number, status: TaxFilingStatus) => {
-        const bracket: Map<[number, number], number> | undefined = brackets.get(status); 
-        if (bracket == undefined) {
-            console.error("Invalid TaxFilingStatus", status);
-            process.exit(1);
+    const find_rate = (income: number, status: TaxFilingStatus): number => {
+        const bracketSet = brackets.get(status); 
+        if (!bracketSet) {
+            throw new Error(`Invalid TaxFilingStatus: ${status}`);
         }
-        bracket.set([lowerbound, upperbound], rate);
-    }
 
-    const find_rate = (income: number, status: TaxFilingStatus) => {
-        const bracket: Map<[number, number], number> | undefined = brackets.get(status); 
-        if (bracket == undefined) {
-            console.error("Invalid TaxFilingStatus", status);
-            process.exit(1);
-        }
-        for (let [key, val] of bracket) {
-            const [min, max] = key;
-            if (min <= income && income <= max) {
-                return val;
+        const N: number = bracketSet.length; 
+        let [l, r] = [0, N - 1];
+        while (l <= r) {
+            const m = l + Math.floor((r - l) / 2);
+            const bracket = bracketSet[m];
+            if (income < bracket.min) {
+                r = m - 1;
+            } else if (income > bracket.max) {
+                l = m + 1;
+            } else {
+                return bracket.rate;
             }
         }
-        // not found
         return -1;
     }
+    const adjust_for_inflation = (rate: number):void => {
+        for (const bracketSet of brackets.values()) {
+            for (const bracket of bracketSet) {
+                bracket.min *= 1 + rate;
+                bracket.max *= 1 + rate;
+            }
+        }
+    }
 
+    const to_string = (): string => {
+        let res = "SINGLE\n";
+        const single_bracket_set = brackets.get(TaxFilingStatus.SINGLE);
+        if (!single_bracket_set) {
+            throw new Error("Missing single taxpayer bracket");
+        }
+        for (const bracket of single_bracket_set) {
+            res += `[${bracket.min}, ${bracket.max}]: ${bracket.rate}\n`
+        }
+        res += "\nMARRIED\n"
+        const married_bracket_set = brackets.get(TaxFilingStatus.MARRIED);
+        if (!married_bracket_set) {
+            throw new Error("Missing married filing jointly taxpayer bracket");
+        }
+        for (const bracket of married_bracket_set) {
+            res += `[${bracket.min}, ${bracket.max}]: ${bracket.rate}\n`
+        }
+        return res;
+    }
     return {
         add_rate,
         find_rate,
         adjust_for_inflation,
-    }
+        to_string,
+    } as const;
 };
 
