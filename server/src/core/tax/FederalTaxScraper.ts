4f3d1743969203d50a7e2e8814d8833c9228538f
Kalvin Liang Wu <kalvinliang123@gmail.com>
2025-03-19 15:17:55 -0400

    standard deduction scraping

--- server/src/core/tax/FederalTaxScraper.ts
+++ server/src/core/tax/FederalTaxScraper.ts
@@ -3,10 +3,14 @@ import "cheerio";
 import { load } from "cheerio";
 import { TaxBrackets, TaxBracketsObject } from "./TaxBrackets";
 import { TaxFilingStatus } from "../Enums";
+import { StandardDeductionObject, StandardDuction } from "./StandardDeduction";
 
-const SINGLE_TABLE: number = 0;
-const MARRIED_TABLE: number = 1;
+const SINGLE_STATUS: number = 0;
+const MARRIED_STATUS: number = 1;
+
 const FEDERAL_TAX_URL: string = "https://www.irs.gov/filing/federal-income-tax-rates-and-brackets";
+const STD_DEDUCTION_URL: string = "https://www.irs.gov/publications/p17";
+const CAPITAL_GAINS_URL: string = "https://www.irs.gov/taxtopics/tc409";
 
 // Function to parse table rows and extract tax bracket data
 function parse_table_rows(taxBrackets: TaxBracketsObject, status: TaxFilingStatus, table: string): void {
@@ -32,12 +36,12 @@ function parse_federal_tax_tables(tables: Array<string>) {
         throw new Error("Not enough table found");
     }
     const taxBrackets = TaxBrackets();    
-    const single_table = tables[SINGLE_TABLE];
+    const single_table = tables[SINGLE_STATUS];
     if (!single_table) {
         throw new Error("Missing single taxpayer table");
     }
     parse_table_rows(taxBrackets, TaxFilingStatus.SINGLE, single_table);
-    const married_table = tables[MARRIED_TABLE];
+    const married_table = tables[MARRIED_STATUS];
     if (!married_table) {
         throw new Error("Misssing married table");
     }
@@ -66,7 +70,58 @@ async function parse_federal_tax_brackets(): Promise<TaxBracketsObject> {
         return parse_federal_tax_tables(federal_tax_tables);
     } catch (error) {
         console.error("Error fetching the federal tax brackets:", error);
-        throw new Error("Federal tax data unavailable. Try again Later");
+        throw new Error("Federal tax data unavailable");
+    }
+}
+
+async function parse_standard_deduction() {
+    try {
+        const { data: html } = await axios.get(STD_DEDUCTION_URL);
+        const $ = load(html);
+
+        const target_text = "Standard deduction amount increased";
+        const paragraph = $("p:contains(" + target_text+ ")");
+        if (!paragraph.length) {
+            throw new Error("No paragraph found contianing target text.");
+        }
+        const itemizedList =  paragraph.next(".itemizedlist").find("ul.itemizedlist");
+        if (!itemizedList) {
+            throw new Error("Itemized list not found");
+        }
+        const std_deductions: StandardDeductionObject = StandardDuction();
+        itemizedList.find("li").each((idx, item) => {
+            const list_item = $(item);
+            const paragraph = list_item.find("p");
+            if (!paragraph) {
+                console.error("Item dont contain a p tag");
+                console.error(list_item.html());
+                return;
+            }
+            const text = paragraph.text();
+            const cost = cleanCurrency(text);
+            if (idx == SINGLE_STATUS) {
+                std_deductions.add_deduction(cost, TaxFilingStatus.SINGLE);
+            } else if (idx == MARRIED_STATUS) {
+                std_deductions.add_deduction(cost, TaxFilingStatus.MARRIED);
+            }
+        });
+        
+        if (std_deductions.size() < 2) {
+            throw new Error("Number of std deduction found is not sufficient");
+        }
+        return std_deductions;
+    } catch (error) {
+        console.error("Error fetching the standard deduction: ", error);
+        throw new Error("Standard deduction data unavailable.");
+    }
+}
+
+async function parse_capital_gains() {
+    try {
+        const { data: html } = await axios.get(CAPITAL_GAINS_URL);
+    } catch (error) {
+        console.error("Error fetching the cpaital gains", error);
+        throw new Error("Capital gains data unavailable.");
     }
 }
 
@@ -77,8 +132,10 @@ const cleanPercentage = (text: string): number =>
     parseFloat(text.replace('%', '')) / 100;
 
 async function main() {
-    const taxBracket = await parse_federal_tax_brackets();
-    console.log(taxBracket.to_string());
+    // const taxBracket = await parse_federal_tax_brackets();
+    // console.log(taxBracket.to_string());
+    const deduction = await parse_standard_deduction();
+    console.log(deduction.to_string());
 } 
 
-main()
+main();
