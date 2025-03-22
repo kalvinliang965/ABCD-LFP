52aaeaf557bad5f162b3dc2c13650d567368234e
Chen Zhu DeskTop_US <zhucoco414@gmail.com>
2025-03-21 20:02:20 -0400

    delete the return value part  and use Enums as the type

--- server/src/core/domain/investment/InvestmentType.ts
+++ server/src/core/domain/investment/InvestmentType.ts
@@ -14,57 +14,33 @@ export class InvestmentType {
   description?: string;
   returnAmtOrPct: ChangeType;
   returnDistributionType: DistributionType;
-  returnValue?: number;
-  returnMean?: number;
-  returnStdev?: number;
-  returnLower?: number;
-  returnUpper?: number;
+  returnDistributionParams: Map<StatisticType, number>;
   expenseRatio: number;
   incomeAmtOrPct: ChangeType;
   incomeDistributionType: DistributionType;
-  incomeValue?: number;
-  incomeMean?: number;
-  incomeStdev?: number;
-  incomeLower?: number;
-  incomeUpper?: number;
+  incomeDistributionParams: Map<StatisticType, number>;
   taxability: Taxability;
 
   constructor(
     name: string,
     returnAmtOrPct: ChangeType,
     returnDistributionType: DistributionType,
+    returnDistributionParams: Map<StatisticType, number>,
     expenseRatio: number,
     incomeAmtOrPct: ChangeType,
     incomeDistributionType: DistributionType,
+    incomeDistributionParams: Map<StatisticType, number>,
     taxability: Taxability,
-    description?: string,
-    returnValue?: number,
-    returnMean?: number,
-    returnStdev?: number,
-    returnLower?: number,
-    returnUpper?: number,
-    incomeValue?: number,
-    incomeMean?: number,
-    incomeStdev?: number,
-    incomeLower?: number,
-    incomeUpper?: number
+    description?: string
   ) {
     this.name = name;
     this.description = description;
     this.returnAmtOrPct = returnAmtOrPct;
     this.returnDistributionType = returnDistributionType;
-    this.returnValue = returnValue;
-    this.returnMean = returnMean;
-    this.returnStdev = returnStdev;
-    this.returnLower = returnLower;
-    this.returnUpper = returnUpper;
+    this.returnDistributionParams = returnDistributionParams;
     this.incomeAmtOrPct = incomeAmtOrPct;
     this.incomeDistributionType = incomeDistributionType;
-    this.incomeValue = incomeValue;
-    this.incomeMean = incomeMean;
-    this.incomeStdev = incomeStdev;
-    this.incomeLower = incomeLower;
-    this.incomeUpper = incomeUpper;
+    this.incomeDistributionParams = incomeDistributionParams;
 
     // Validate expense ratio is non-negative
     if (expenseRatio < 0) {
@@ -81,25 +57,9 @@ export class InvestmentType {
    * @returns The generated annual return value
    */
   generateExpectedAnnualReturn(baseAmount?: number): number {
-    const params = new Map<StatisticType, number>();
-
-    switch (this.returnDistributionType) {
-      case DistributionType.FIXED:
-        params.set(StatisticType.VALUE, this.returnValue || 0);
-        break;
-      case DistributionType.NORMAL:
-        params.set(StatisticType.MEAN, this.returnMean || 0);
-        params.set(StatisticType.STDDEV, this.returnStdev || 0);
-        break;
-      case DistributionType.UNIFORM:
-        params.set(StatisticType.LOWER, this.returnLower || 0);
-        params.set(StatisticType.UPPER, this.returnUpper || 0);
-        break;
-    }
-
     const returnValue = ValueGenerator(
       this.returnDistributionType,
-      params
+      this.returnDistributionParams
     ).sample();
 
     // If return is specified as percentage and baseAmount is provided, calculate the actual amount
@@ -119,25 +79,9 @@ export class InvestmentType {
    * @returns The generated annual income value
    */
   generateExpectedAnnualIncome(baseAmount?: number): number {
-    const params = new Map<StatisticType, number>();
-
-    switch (this.incomeDistributionType) {
-      case DistributionType.FIXED:
-        params.set(StatisticType.VALUE, this.incomeValue || 0);
-        break;
-      case DistributionType.NORMAL:
-        params.set(StatisticType.MEAN, this.incomeMean || 0);
-        params.set(StatisticType.STDDEV, this.incomeStdev || 0);
-        break;
-      case DistributionType.UNIFORM:
-        params.set(StatisticType.LOWER, this.incomeLower || 0);
-        params.set(StatisticType.UPPER, this.incomeUpper || 0);
-        break;
-    }
-
     const incomeValue = ValueGenerator(
       this.incomeDistributionType,
-      params
+      this.incomeDistributionParams
     ).sample();
 
     // If income is specified as percentage and baseAmount is provided, calculate the actual amount
@@ -165,7 +109,7 @@ export class InvestmentType {
       ? Taxability.TAXABLE
       : Taxability.TAX_EXEMPT;
 
-    // Determine distribution type and parameters
+    // Determine return distribution type and create parameter map
     const returnDistType =
       data.returnDistribution.get("type") === "fixed"
         ? DistributionType.FIXED
@@ -173,6 +117,49 @@ export class InvestmentType {
         ? DistributionType.NORMAL
         : DistributionType.UNIFORM;
 
+    const returnParams = new Map<StatisticType, number>();
+
+    // Add relevant parameters based on distribution type
+    switch (returnDistType) {
+      case DistributionType.FIXED:
+        if (data.returnDistribution.get("value") !== undefined) {
+          returnParams.set(
+            StatisticType.VALUE,
+            data.returnDistribution.get("value")
+          );
+        }
+        break;
+      case DistributionType.NORMAL:
+        if (data.returnDistribution.get("mean") !== undefined) {
+          returnParams.set(
+            StatisticType.MEAN,
+            data.returnDistribution.get("mean")
+          );
+        }
+        if (data.returnDistribution.get("stdev") !== undefined) {
+          returnParams.set(
+            StatisticType.STDDEV,
+            data.returnDistribution.get("stdev")
+          );
+        }
+        break;
+      case DistributionType.UNIFORM:
+        if (data.returnDistribution.get("lower") !== undefined) {
+          returnParams.set(
+            StatisticType.LOWER,
+            data.returnDistribution.get("lower")
+          );
+        }
+        if (data.returnDistribution.get("upper") !== undefined) {
+          returnParams.set(
+            StatisticType.UPPER,
+            data.returnDistribution.get("upper")
+          );
+        }
+        break;
+    }
+
+    // Determine income distribution type and create parameter map
     const incomeDistType =
       data.incomeDistribution.get("type") === "fixed"
         ? DistributionType.FIXED
@@ -180,25 +167,59 @@ export class InvestmentType {
         ? DistributionType.NORMAL
         : DistributionType.UNIFORM;
 
+    const incomeParams = new Map<StatisticType, number>();
+
+    // Add relevant parameters based on distribution type
+    switch (incomeDistType) {
+      case DistributionType.FIXED:
+        if (data.incomeDistribution.get("value") !== undefined) {
+          incomeParams.set(
+            StatisticType.VALUE,
+            data.incomeDistribution.get("value")
+          );
+        }
+        break;
+      case DistributionType.NORMAL:
+        if (data.incomeDistribution.get("mean") !== undefined) {
+          incomeParams.set(
+            StatisticType.MEAN,
+            data.incomeDistribution.get("mean")
+          );
+        }
+        if (data.incomeDistribution.get("stdev") !== undefined) {
+          incomeParams.set(
+            StatisticType.STDDEV,
+            data.incomeDistribution.get("stdev")
+          );
+        }
+        break;
+      case DistributionType.UNIFORM:
+        if (data.incomeDistribution.get("lower") !== undefined) {
+          incomeParams.set(
+            StatisticType.LOWER,
+            data.incomeDistribution.get("lower")
+          );
+        }
+        if (data.incomeDistribution.get("upper") !== undefined) {
+          incomeParams.set(
+            StatisticType.UPPER,
+            data.incomeDistribution.get("upper")
+          );
+        }
+        break;
+    }
+
     return new InvestmentType(
       data.name,
       convertAmtOrPct(data.returnAmtOrPct),
       returnDistType,
+      returnParams,
       data.expenseRatio,
       convertAmtOrPct(data.incomeAmtOrPct),
       incomeDistType,
+      incomeParams,
       taxabilityEnum,
-      data.description,
-      data.returnDistribution.get("value"),
-      data.returnDistribution.get("mean"),
-      data.returnDistribution.get("stdev"),
-      data.returnDistribution.get("lower"),
-      data.returnDistribution.get("upper"),
-      data.incomeDistribution.get("value"),
-      data.incomeDistribution.get("mean"),
-      data.incomeDistribution.get("stdev"),
-      data.incomeDistribution.get("lower"),
-      data.incomeDistribution.get("upper")
+      data.description
     );
   }
 }
