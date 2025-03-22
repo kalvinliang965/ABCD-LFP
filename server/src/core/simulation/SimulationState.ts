8894d15760b1f03d60e4686e48808ea4d4cb79eb
Kalvin Liang Wu <kalvinliang123@gmail.com>
2025-03-21 20:23:10 -0400

    finish most of simulation state

--- server/src/core/simulation/SimulationState.ts
+++ server/src/core/simulation/SimulationState.ts
@@ -1,5 +1,4 @@
-import { start } from "repl";
-import Scenario, { ScenarioReturnType } from "../domain/scenario/Scenario";
+import { ScenarioReturnType } from "../domain/scenario/Scenario";
 import { TaxFilingStatus } from "../Enums";
 
 function SimulationState(
@@ -54,10 +53,26 @@ function SimulationState(
     const roth_conversion_end = scenario.roth_conversion_end;
     const roth_conversion_strategy = scenario.roth_conversion_strategy;
 
+    // same as ordinary income, but because bracket use taxable we will also use it 
     let taxable_income = 0;
+    const incr_taxable_income = (amt: number) => {
+        taxable_income += amt;
+    }
     let capital_gains_income = 0;
+    const incr_capital_gains_income = (amt: number) => {
+        capital_gains_income += amt;
+    }   
     let social_security_income = 0;
-    
+    const incr_social_security_income = (amt: number) => {
+        social_security_income += amt;
+    }
+
+    const setup = () => {
+        taxable_income = 0;
+        capital_gains_income = 0;
+        social_security_income = 0;
+        // TODO
+    }
 
     return {
         start_year,
@@ -75,6 +90,7 @@ function SimulationState(
         spouse_age,
         spouse_year_of_death,
         is_spouse_alive,   
+        setup,
     }
 }
 
