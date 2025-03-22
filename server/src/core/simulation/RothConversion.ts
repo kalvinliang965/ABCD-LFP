0f86b4f9e6e93d5de6da63593062866466349b68
Kalvin Liang Wu <kalvinliang123@gmail.com>
2025-03-22 11:52:42 -0400

    defined interface

--- server/src/core/simulation/RothConversion.ts
+++ server/src/core/simulation/RothConversion.ts
@@ -1,1 +1,10 @@
 // src/core/simulation/RothConversion.ts
+
+import SimulationState, {SimulationStateObject} from "./SimulationState";
+
+function process_roth_conversion(simulation_state: SimulationStateObject) {
+
+
+}
+
+export default process_roth_conversion;
