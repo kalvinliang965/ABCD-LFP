368b6f47ab9198e0cba2d8dd8e53996a876ff1e8
Chen Zhu DeskTop_US <zhucoco414@gmail.com>
2025-03-30 12:36:58 -0400

    api.ts need help for the port, front end changed

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { Investment } from "../types/investment";
+import { config } from "../config";
 
 // API base URL
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+//! Need help!!!!!!
+const API_URL =
+  import.meta.env.VITE_API_URL || `http://localhost:${config.apiPrefix}/api`;
 
 /**
  * Map frontend Investment model to backend InvestmentType model
