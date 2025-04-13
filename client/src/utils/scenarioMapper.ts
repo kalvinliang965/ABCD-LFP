06e2b01a9ddb600d77ac11a4b6b46df74f00e7ee
Ekaterina Tkachenko <etkachenko.ok@gmail.com>
2025-04-13 14:37:49 -0400

    fixed marital status typo

--- client/src/utils/scenarioMapper.ts
+++ client/src/utils/scenarioMapper.ts
@@ -199,7 +199,7 @@ export function map_form_to_scenario_raw(
   // Return the final ScenarioRaw object
   return {
     name: scenarioDetails.name,
-    martialStatus: scenarioDetails.type === "couple" ? "couple" : "individual",
+    maritalStatus: scenarioDetails.type === "couple" ? "couple" : "individual",
     birthYears,
     lifeExpectancy,
     investmentTypes,
